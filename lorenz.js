b5c8736b289d5ef06c0cd162535f414cba17ed03
Jim Traugot <jtraug@gmail.com>
2015-09-14 09:58:29 -0700

    Add dt=0.01

--- lorenz.js
+++ lorenz.js
@@ -1,22 +1,36 @@
 // Loenz strange attractors:
 
 
+
+
+
+
 var Lorenz = function (Xinit, Yinit, Zinit) {
 this.x = Xinit;
 this.y = Yinit;
 this.z = Zinit;
 this.rho = 28;
 this.sigma = 10;
-this.beta = 2.66667; //8/3;
+this.beta = 8/3;
+this.dt = 0.01;
 
 this.getDx = function() {
-        return ( this.sigma * (this.nexty - this.nextx));
+    var answer =  ( this.sigma * (this.nexty - this.nextx));
+    console.log ("dx/dt = " + answer);
+    return this.dt * answer;
 };
 this.getDy= function() {
-        return( this.nextx * (this.rho - this.nextz) -this.nexty);
+   var answer = ( this.nextx * (this.rho - this.nextz) -this.nexty);
+    //    var answer = this.rho * (this.x - this.y - (this.x * this.z)); 
+    console.log ("dy/dt = " + answer);
+    return this.dt * answer;
+
 };
 this.getDz = function() {
-    return (this.nextx * this.nexty - (this.beta  * this.nextz));
+    var answer =  (this.nextx * this.nexty - (this.beta  * this.nextz));
+    console.log ("dz/dt = " + answer);
+    return this.dt * answer;
+
 };
 
 };
@@ -27,9 +41,9 @@ Lorenz.prototype.getNextPoint = function() {
     this.nextz = this.z;
    // console.log("this.nextx = " + this.nextx + " this.nexty = " + this.nexty + " this.nextz = " + this.nextz);
 
-    this.x -= this.getDx();
-    this.y -= this.getDy();
-    this.z -= this.getDz();
+    this.x += this.getDx();
+    this.y += this.getDy();
+    this.z += this.getDz();
     //console.log("this.nextx = " + this.nextx + " this.nexty = " + this.nexty + " this.nextz = " + this.nextz);
     return[this.nextx, this.nexty, this.nextz];
 };
