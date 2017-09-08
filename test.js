5ae3d9a2a9441ab026e8b4b675ca5cd56e6970cc
jim traugott <jtraug@gmail.com>
2017-09-07 20:51:29 -0700

    removed test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var Lorenz = require('./lorenz');
-
-var lor = new Lorenz(1,1,1);
-
-for (var i=0; i< 10; ++i) {
-    var point = lor.getNextPoint();
-    console.log ('point: x = ' + point[0] + ' y= ' + point[1] + ' z= ' + point[2]);
-}
