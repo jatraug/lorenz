861f3c1d8d0d9d2d1508240c0ce691ed7121ce55
jim traugott <jtraug@gmail.com>
2017-09-08 16:40:54 -0700

    Change name of dt to scaler

--- lorenz.js
+++ lorenz.js
@@ -12,24 +12,24 @@ var Lorenz = function (Xinit, Yinit, Zinit) {
     this.rho = 28;
     this.sigma = 10;
     this.beta = 8/3;
-    this.dt = 0.01;
+    this.scaler = 0.01;
 
     this.getDx = function () {
         var answer =  ( this.sigma * (this.nexty - this.nextx));
         //console.log ("dx/dt = " + answer);
-        return this.dt * answer;
+        return this.scaler * answer;
     };
     this.getDy = function () {
         var answer = ( this.nextx * (this.rho - this.nextz) -this.nexty);
         //    var answer = this.rho * (this.x - this.y - (this.x * this.z)); 
         //    console.log ("dy/dt = " + answer);
-        return this.dt * answer;
+        return this.scaler * answer;
 
     };
     this.getDz = function () {
         var answer =  (this.nextx * this.nexty - (this.beta  * this.nextz));
         //    console.log ("dz/dt = " + answer);
-        return this.dt * answer;
+        return this.scaler * answer;
 
     };
 
